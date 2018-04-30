f25d5070174e0ad6ee99332a0198ce38e75af175
training135 <training135>
2018-04-30 14:23:34 +0530

    Initiated circle ci with Dotnet Console

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotnettest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! here");
+            Console.WriteLine("Hello again from console");
+        }
+    }
+}
